53e187a1e84d659d83f87f866054d59a953e05f6
Rayán <rbelhajjannus@cifpfbmoll.eu>
2024-12-31 15:34:48 +0100

    Update BinaryRegexp.java

--- BinaryRegexp.java
+++ BinaryRegexp.java
@@ -4,6 +4,6 @@ import java.util.regex.Pattern;
 public class BinaryRegexp {
   public static Pattern multipleOf3() {
     // Regular expression that matches binary inputs that are multiple of 3
-    return Pattern.compile("^0*((1(0(1)*0)*1)*0*)*$");
+    return Pattern.compile("^((1(0(1)*0)*1)*0*)*$");
   }
 }
